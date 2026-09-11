3447713b21ee65399f38c85afa4515cd168f86ff
CH.Prasanna Lakshmi <ch.prasannalakshmi@CHPrasannas-MacBook-Neo.local>
2026-09-11 12:46:07 +0530

    fix(security): restrict ssh access

--- infra/main.tf
+++ infra/main.tf
@@ -52,7 +52,7 @@ resource "aws_security_group" "api" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["10.0.0.0/16"]
   }
 
   ingress {
@@ -66,7 +66,7 @@ resource "aws_security_group" "api" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["10.0.0.0/16"]
   }
 }
 
